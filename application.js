f7705c656b43aafd497e560d9cfcbe3ea28a92e5
wpprpiqpgithub <131727260+wpprpiqpgithub@users.noreply.github.com>
2023-05-15 10:52:45 +0800

    Add files via upload

---
+++ application.js
@@ -0,0 +1,19 @@
+
+var bth = document.getElementById('button');
+var image = document.querySelector(".pictureOne");
+
+var y =0;
+
+
+
+    image.onclick = function() {
+            y+=40;
+            image.style.top = y + "px";
+        
+        }
+
+    
+    bth.onclick = function() {
+        y-=40;
+        image.style.top = y + "px";
+    }
